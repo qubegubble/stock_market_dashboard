e627c03242f72eb925184c49be59806fd3fbd913
qubegubble <104300719+qubegubble@users.noreply.github.com>
2025-02-06 08:10:12 +0000

    Deploying to gh-pages from @ qubegubble/stock_market_dashboard@9f2200646e4f14c1ee9e9b37037c5c8d7d4a9978 🚀

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: '/stock_market_dashboard',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: '/stock_market_dashboard/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/stock_market_dashboard"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 493, hash: 'd250a8cc158d95f402467a433415ba7bc8235263226cf36404746c75a6d5432b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1006, hash: '7e594f7fe45ab4a244d9efa1db1a5148d09670111fc38aa337f9336540a69625', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 20834, hash: '1af781a0aaa347d7cc0e7fff78d39652cb68c5f4cf19c94184152764add11665', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 516, hash: '18566a1ced5983492bd2d381f5904d948049042d2a4e2e0422c14f35c3934e69', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1029, hash: 'ed8402fff018b0cdb2f5d55416af3093b6d1311686cb1b3bf57e2701adde5c3f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 20857, hash: '5fd1bf7be444dbe07fd6daf385103e98b6aef44efd4c5a88b5bdbe485012db28', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
