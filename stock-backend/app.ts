095d6ab1cab7c231ca47218514bf05b7e5402f91
Leo Hrvat <leo.hrvat@stud.hslu.ch>
2025-02-21 10:28:45 +0100

    Complete Refactoring

--- stock-backend/app.ts
+++ stock-backend/app.ts
@@ -1,16 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
-import stockRoutes from "./src/routes/stock.routes";
+import express, {Express, Request} from 'express';
 
-dotenv.config();
+const app: Express = express();
 
-const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(morgan('dev'));
 
-app.use('/api/stocks', stockRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
 export default app;
--- stock-backend/server.ts
+++ stock-backend/server.ts
@@ -1,23 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import stockRoutes from './src/routes/stock.routes';
+import app from './app';
 
-require('dotenv').config();
+const port: number = 3000;
 
-const app = express();
-const PORT = process.env.PORT || 5000 as number;
-const MONGODB_URI = process.env.MONGODB_URI as string;
-
-app.use(express.json());
-app.use(cors());
-app.use('/api/stocks', stockRoutes);
-
-mongoose.connect(MONGODB_URI, {
-
-}).then(() => { console.log('Connected to MongoDB') }).catch((err) => { console.log(err) });
-
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
