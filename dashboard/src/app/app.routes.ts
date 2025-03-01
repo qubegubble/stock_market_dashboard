e0414ee2479dcb2b700297c3341fb7efea89fd52
Leo Hrvat <leo.hrvat@stud.hslu.ch>
2025-03-01 16:04:15 +0100

    some more backend stuff

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(routes), RegisterComponent, LoginComponent, DashboardComponent, BrowserModule, CommonModule, FormsModule],
+  providers: [],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- stock-backend/app.ts
+++ stock-backend/app.ts
@@ -1,11 +1,31 @@
-import express, {Express, Request} from 'express';
+// src/app.ts
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerSpec } from './src/swagger';
+import stockRouter from './src/controllers/stock.controller';
+import authRouter from './src/controllers/auth.controller';
+import mongoose from "mongoose";
 
-const app: Express = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
+const mongoDb = process.env.MONGODB_URI || 'mongodb://localhost/stockdb';
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+// Serve the Swagger docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Mount your routes
+app.use('/api/stocks', stockRouter, authRouter);
+
+mongoose.connect(mongoDb, {
+}).then(() => {
+  console.log("Connected to MongoDB")
+}).catch((err) => {
+  console.error(`Failed to connect to MongoDB: ${err}`);
 });
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 export default app;
